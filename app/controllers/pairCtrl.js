6876c41c0427b63e7d6a45e31317afa2831cc1a0
Jack Mocherman <mochermanjack@gmail.com>
2016-08-03 09:37:52 -0500

    pair button makes call to local json and returns tags that correspond with subjects

--- app/controllers/pairCtrl.js
+++ app/controllers/pairCtrl.js
@@ -1,12 +1,18 @@
 'use strict';
 
-app.controller('pairCtrl', function($scope, bookFactory, $rootScope) {
+app.controller('pairCtrl', function($scope, bookFactory, $rootScope, pairFactory) {
 
   $scope.album = false;
+  $scope.selectedBook = null;
 
   $scope.pair = function() {
     $scope.selectedBook = bookFactory.getSelectedBook();
-    console.log("", $scope.selectedBook);
+    pairFactory.getSubjectNames($scope.selectedBook.subjects)
+      .then((albumTagArray) => {
+        console.log(albumTagArray);
+      });
   };
 
 });
+
+// array sort method
